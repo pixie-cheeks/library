496a9bd1104a0505308bbf0a1bbcf3c22ad6992d
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2023-09-08 20:15:16 +0530

    Save code before JS reset

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 const myLibrary = [];
@@ -10,57 +10,66 @@ const addButton = document.querySelector('.add-book');
 const booksGrid = document.querySelector('.books-grid');
 const dialog = document.querySelector('dialog');
 const closeDialogBtn = document.querySelector('.close-button');
+const dialogSubmit = document.querySelector('.dialog-submit');
 
-addButton.addEventListener('click', getBookFromUser);
+addButton.addEventListener('click', () => dialog.showModal());
 closeDialogBtn.addEventListener('click', () => dialog.close());
+dialogSubmit.addEventListener('click', addBookToLibrary);
 
-function addBookToLibrary(bookObject) {
-  myLibrary.push(bookObject);
+const title = dialog.querySelector('#title');
+const author = dialog.querySelector('#author');
+const pages = dialog.querySelector('#pages');
+const read = dialog.querySelector('#read');
+
+function addBookToLibrary() {
+    console.log(title.value);
 }
 
-function getBookFromUser() {
-  dialog.showModal();
+/* function addBookToLibrary(bookObject) {
+    myLibrary.push(bookObject);
+}
+
+function getBookFromUser(event) {
+    event.preventDefault();
+    const title = dialog.querySelector('#title');
+    const author = dialog.querySelector('#author');
+    const pages = dialog.querySelector('#pages');
+    const haveRead = dialog.querySelector('#read-status');
+
+    addBookToLibrary(
+        new Book(title.value, author.value, pages.value, haveRead.checked)
+    );
+    dialog.close();
+
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    haveRead.checked = false;
+
+    displayBooks();
 }
 
 function displayBooks() {
-  const template = document.querySelector('.template');
+    const template = document.querySelector('.template');
 
-  for (let i = 0; i < myLibrary.length; i++) {
-    const newBook = createBookCard(template, myLibrary[i]);
-    booksGrid.appendChild(newBook);
-  }
+    for (let i = booksGrid.childElementCount; i <= myLibrary.length; i++) {
+        const newBook = createBookCard(template, myLibrary[i]);
+        // newBook.dataset.index = i;
+        booksGrid.appendChild(newBook);
+    }
 }
 
 function createBookCard(template, bookInfo) {
-  const bookCard = template.cloneNode(true);
-  bookCard.className = 'book-card';
+    const bookCard = template.cloneNode(true);
+    bookCard.className = 'book-card';
 
-  bookCard.querySelector('.author')
-    .textContent = bookInfo.author;
-  bookCard.querySelector('.title')
-    .textContent = bookInfo.title;
-  bookCard.querySelector('.pages')
-    .textContent = bookInfo.pages;
+    console.log(bookInfo, bookInfo.author);
+    bookCard.querySelector('.author')
+        .textContent = bookInfo.author;
+    bookCard.querySelector('.title')
+        .textContent = bookInfo.title;
+    bookCard.querySelector('.pages')
+        .textContent = bookInfo.pages;
 
-  return bookCard;
-}
-
-// To be removed
-/* const removeLastBtn = document.querySelector('.remove-book');
-
-addButton.addEventListener('click', addDiv);
-removeLastBtn.addEventListener('click', removeLastDiv);
-
-function addDiv() {
-  const div = document.createElement('div');
-
-  booksGrid.appendChild(div);
-}
-
-function removeLastDiv() {
-  const lastDiv = booksGrid.querySelector(':scope > div:last-child');
-
-  if (lastDiv) {
-    lastDiv.remove();
-  }
+    return bookCard;
 } */
